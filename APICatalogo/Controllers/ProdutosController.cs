cfa9c01abd983a7e37949b85f49d6fae079e0ef5
Lucasbomgon <lucas.bom.gon2132@gmail.com>
2026-03-12 15:44:16 -0300

    commit APICatalogo

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,42 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace APICatalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos =_context.Produtos.ToList();
+            if (produtos is null)
+            {
+                return NotFound("Produtos nao encontrados...");
+            }
+            return produtos;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is  null) 
+            {
+                return NotFound("");
+            }
+            return produto;
+
+    }
+}
---
+++ APICatalogo/Migrations/20260309121509_PopulaProdutos.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos(ProdutoId,Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" + 
+                "Values('1','Coca-Cola Diet','Refrigerante de Cola 350ml',5.45,'cocacola.jpg',50,now(),1)");
+
+            mb.Sql("Insert into Produtos(ProdutoId,Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" +  
+                "Values('2','Lanche de doritos','Lanche de doritos com cheddar',9.50,'doritos.jpg',10,now(),2)");
+
+            mb.Sql("Insert into Produtos(ProdutoID,Nome,Descricao,Preco,ImagemUrl,Estoque,DataCadastro,CategoriaId)" + 
+                "Values('3','Ouro branco','Ouro Branco bombom',3.30,'ourobranco.jpg',20,now(),3)");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
