0be4367429821c314d67a0d87b531c95868a075a
Lucasbomgon <lucas.bom.gon2132@gmail.com>
2026-03-25 10:31:30 -0300

    Metodos Actions Assincornos

--- obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4752eca0cedb279156c5f021bc2ff1d07455e25c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5713092be9707b64858bd4401622d8784f180d36")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
